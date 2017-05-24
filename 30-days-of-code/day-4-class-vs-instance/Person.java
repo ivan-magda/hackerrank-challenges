54b7639313cab9864aa86a9c6e9ea8ee5cfd5ddd
Ivan Magda <imagda15@gmail.com>
2017-05-24 17:05:20 +0900

    [30-DC] feat: Add `Day 4: Class vs. Instance` solution

---
+++ 30-days-of-code/day-4-class-vs-instance/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+    private int age;	
+  
+	public Person(int initialAge) {
+  		// Add some more code to run some checks on initialAge
+        if (initialAge < 0) {
+            this.age = 0;
+            System.out.println("Age is not valid, setting age to 0.");
+        } else {
+            this.age = initialAge;
+        }
+	}
+
+	public void amIOld() {
+  		if (age < 13) {
+            System.out.println("You are young.");
+        } else if (age < 18) {
+            System.out.println("You are a teenager.");
+        } else {
+            System.out.println("You are old.");
+        }
+	}
+
+	public void yearPasses() {
+  		// Increment this person's age.
+        this.age++;
+	}
+}
---
+++ 30-days-of-code/day-4-class-vs-instance/README.md
@@ -0,0 +1,3 @@
+# Day 4: Class vs. Instance.
+
+[Problem description](https://www.hackerrank.com/challenges/30-class-vs-instance)
