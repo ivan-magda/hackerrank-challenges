5bbf68cec63e571d6af20239a330311e45c15720
Ivan Magda <imagda15@gmail.com>
2017-05-24 16:23:39 +0900

    [cpp] feat: Add `Conditional Statements` solution

---
+++ cpp/introduction/conditional-statements/README.md
@@ -0,0 +1,3 @@
+# Conditional Statements
+
+[Problem description](https://www.hackerrank.com/challenges/c-tutorial-conditional-if-else)
---
+++ cpp/introduction/conditional-statements/main.cpp
@@ -0,0 +1,65 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    
+        switch (n) {
+            case 1:
+                cout << "one";
+                break;
+            case 2:
+                cout << "two";
+                break;
+            case 3:
+                cout << "three";
+                break;
+            case 4:
+                cout << "four";
+                break;
+            case 5:
+                cout << "five";
+                break;
+            case 6:
+                cout << "six";
+                break;
+            case 7:
+                cout << "seven";
+                break;
+            case 8:
+                cout << "eight";
+                break;
+            case 9:
+                cout << "nine";
+                break;
+            default:
+                cout << "Greater than 9";
+                break;
+        }
+    
+    return 0;
+}
