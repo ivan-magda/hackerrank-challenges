598d94ec5530311eae798f70d75a449d678943bf
Ivan Magda <imagda15@gmail.com>
2017-05-28 13:25:55 +0900

    [30-DC] feat: Add `Day 13: Abstract Classes` solution

---
+++ 30-days-of-code/day-13-abstract-classes/README.md
@@ -0,0 +1,3 @@
+# Day 13: Abstract Classes.
+
+[Problem description](https://www.hackerrank.com/challenges/30-abstract-classes)
---
+++ 30-days-of-code/day-13-abstract-classes/Solution.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+abstract class Book {
+    String title;
+    String author;
+    
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+    
+    abstract void display();
+}
+
+class MyBook extends Book {
+    private int price;
+ 
+   /**   
+    *   Class Constructor
+    *   
+    *   @param title The book's title.
+    *   @param author The book's author.
+    *   @param price The book's price.
+    **/
+    MyBook(String title, String author, int price) {
+        super(title, author);
+        this.price = price;
+    }
+ 
+   /**   
+    *   Method Name: display
+    *   
+    *   Print the title, author, and price in the specified format.
+    **/
+    void display() {
+        System.out.println("Title: " + this.title);
+        System.out.println("Author: " + this.author);
+        System.out.println("Price: " + this.price);
+    }
+}
+
+public class Solution {
+   
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        String author = scanner.nextLine();
+        int price = scanner.nextInt();
+        scanner.close();
+
+        Book book = new MyBook(title, author, price);
+        book.display();
+    }
+}
