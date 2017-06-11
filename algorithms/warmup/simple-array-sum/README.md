93d0e20323504f775dde80e9a6be220c9d50312b
Ivan Magda <imagda15@gmail.com>
2017-06-11 13:01:24 +0900

    [algorithms] feat: Add `Simple Array Sum` solution

---
+++ algorithms/warmup/simple-array-sum/README.md
@@ -0,0 +1,3 @@
+# Simple Array Sum.
+
+[Problem description](https://www.hackerrank.com/challenges/simple-array-sum)
---
+++ algorithms/warmup/simple-array-sum/Solution.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int simpleArraySum(int n, int[] ar) {
+        int sum = 0;
+        
+        for (int element : ar) {
+            sum += element;
+        }
+        
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] ar = new int[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextInt();
+        }
+        int result = simpleArraySum(n, ar);
+        System.out.println(result);
+    }
+}
