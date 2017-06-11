21cc997d9030f0b4af1b7631c6a587547fd6ed60
Ivan Magda <imagda15@gmail.com>
2017-06-11 13:06:51 +0900

    [algorithms] feat: Add `Compare the Triplets` solution

---
+++ algorithms/warmup/compare-the-triplets/README.md
@@ -0,0 +1,3 @@
+# Compare the Triplets.
+
+[Problem description](https://www.hackerrank.com/challenges/compare-the-triplets)
---
+++ algorithms/warmup/compare-the-triplets/Solution.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int[] solve(int a0, int a1, int a2, int b0, int b1, int b2){
+        int aliceScore = 0;
+        int bobScore = 0;
+        
+        if (a0 > b0) {
+            aliceScore++;
+        } else if (b0 > a0) {
+            bobScore++;
+        }
+        
+        if (a1 > b1) {
+            aliceScore++;
+        } else if (b1 > a1) {
+            bobScore++;
+        }
+        
+        if (a2 > b2) {
+            aliceScore++;
+        } else if (b2 > a2) {
+            bobScore++;
+        }
+        
+        return new int[]{aliceScore, bobScore};
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a0 = in.nextInt();
+        int a1 = in.nextInt();
+        int a2 = in.nextInt();
+        int b0 = in.nextInt();
+        int b1 = in.nextInt();
+        int b2 = in.nextInt();
+        int[] result = solve(a0, a1, a2, b0, b1, b2);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + (i != result.length - 1 ? " " : ""));
+        }
+        System.out.println("");
+        
+
+    }
+}
