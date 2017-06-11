e49c58312b71b7cbbce941370d045b9817f54e01
Ivan Magda <imagda15@gmail.com>
2017-06-12 01:57:09 +0900

    [algorithms] feat: Add `Mini-Max Sum` solution

---
+++ algorithms/warmup/mini-max-sum/README.md
@@ -0,0 +1,3 @@
+# Mini-Max Sum
+
+[Problem description](https://www.hackerrank.com/challenges/mini-max-sum)
---
+++ algorithms/warmup/mini-max-sum/Solution.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class Main {
+
+    private static final int NUMBERS_COUNT = 5;
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        long[] numbers = new long[NUMBERS_COUNT];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = in.nextLong();
+        }
+        in.close();
+
+        long max = Long.MIN_VALUE;
+        long min = Long.MAX_VALUE;
+
+        for (int i = 0; i < numbers.length; i++) {
+            long currentSum = 0;
+
+            for (int j = 0; j < numbers.length; j++) {
+                if (i == j) continue;
+                currentSum += numbers[j];
+            }
+
+            if (currentSum > max) {
+                max = currentSum;
+            }
+            if (currentSum < min) {
+                min = currentSum;
+            }
+        }
+
+        System.out.println(String.valueOf(min) + " " + String.valueOf(max));
+    }
+}
