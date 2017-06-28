bc54fb664c5af8a362bc09fb5dad17aee6dd0e06
Ivan Magda <imagda15@gmail.com>
2017-06-28 15:36:58 +0300

    [30-DC] chore: code cleanup

--- 30-days-of-code/day-25-running-time-and-complexity/Solution.java
+++ 30-days-of-code/day-25-running-time-and-complexity/Solution.java
@@ -10,7 +10,9 @@ class Solution {
         }
 
         for (int i = 2; i * i <= number; i++) {
-            if (number % i == 0) return false;
+            if (number % i == 0) {
+                return false;
+            }
         }
 
         return true;
