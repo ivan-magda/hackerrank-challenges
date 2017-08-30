82f580d61c3f3e7e77597906ea21f214d72d9d70
Ivan Magda <imagda15@gmail.com>
2017-08-30 23:38:19 +0300

    [practice-problems] chore: Add additional solution
    Add additional linear time complexity solution for rearrange-the-array-with-alternate-high-and-low-elements problem

--- src/practice-problems/array/rearrange-the-array-with-alternate-high-and-low-elements/Main.java
+++ src/practice-problems/array/rearrange-the-array-with-alternate-high-and-low-elements/Main.java
@@ -33,8 +33,38 @@ public class Main {
         return rearranged;
     }
 
+    /**
+     * Function to rearrange the array such that every second element
+     * of the array is greater than its left and right elements.
+     */
+    private static void rearrangeArray(int array[]) {
+        // start from second element and increment index
+        // by 2 for each iteration of loop.
+        for (int i = 1; i < array.length; i += 2) {
+            // If the prev element is greater than current element, swap the elements.
+            if (array[i - 1] > array[i]) {
+                swap(array, i - 1, i);
+            }
+
+            // if next element is greater than current element, swap the elements.
+            if (i + 1 < array.length && array[i + 1] > array[i]) {
+                swap(array, i + 1, i);
+            }
+        }
+    }
+
+    private static void swap(int[] array, int lhs, int rhs) {
+        int temp = array[lhs];
+        array[lhs] = array[rhs];
+        array[rhs] = temp;
+    }
+
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 4, 5, 6, 7};
         System.out.println("Rearranged: " + Arrays.toString(rearranged(array)));
+
+        int secondArray[] = {9, 6, 8, 3, 7};
+        rearrangeArray(secondArray);
+        System.out.println("Rearranged: " + Arrays.toString(secondArray));
     }
 }
