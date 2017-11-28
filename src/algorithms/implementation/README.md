5770ad06285db95ff429096c684f2fee697e50eb
Ivan Magda <imagda15@gmail.com>
2017-11-28 22:51:25 +0300

    [algorithms/implementation] chore: Solve the-birthday-bar

--- src/algorithms/implementation/README.md
+++ src/algorithms/implementation/README.md
@@ -7,3 +7,4 @@
 - [Kangaroo](./kangaroo)
 - [Between Two Sets](./between-two-sets)
 - [Breaking the Records](./breaking-best-and-worst-records)
+- [Birthday Chocolate](./the-birthday-bar)
---
+++ src/algorithms/implementation/the-birthday-bar/Solution.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    private static int solve(int length, int[] values, int targetSum, int targetConsecutiveItems) {
+        int res = 0;
+
+        for (int i = 0; i < length; i++) {
+            int sum = 0;
+            int countConsecutive = 1;
+            for (int j = i; countConsecutive <= targetConsecutiveItems && j < length; j++, countConsecutive++) {
+                sum += values[j];
+                if (sum == targetSum && countConsecutive == targetConsecutiveItems) {
+                    res++;
+                }
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Res = " + solve(5, new int[]{1, 2, 1, 3, 2}, 3, 2));
+        System.out.println("Res = " + solve(6, new int[]{1, 1, 1, 1, 1, 1}, 3, 2));
+        System.out.println("Res = " + solve(1, new int[]{4}, 4, 1));
+    }
+}
