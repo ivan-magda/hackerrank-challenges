5c0bad397712309130e12c80e83ce1294c260f6d
Ivan Magda <imagda15@gmail.com>
2017-12-21 13:32:56 +0300

    [code-reference/RBTree] chore: Log empty tree

--- src/code-references/IMRedBlackTree/src/Main.java
+++ src/code-references/IMRedBlackTree/src/Main.java
@@ -1,6 +1,9 @@
+import tree.IMRedBlackTree;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        IMRedBlackTree tree = new IMRedBlackTree();
+        System.out.println(tree);
     }
 }
