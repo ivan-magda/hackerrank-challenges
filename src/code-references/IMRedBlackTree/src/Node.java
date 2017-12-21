c08fcfe1b88c2974016d85f29cd18978b86a5819
Ivan Magda <imagda15@gmail.com>
2017-12-21 13:27:36 +0300

    [code-reference/RBTree] chore: Create Node class

---
+++ src/code-references/IMRedBlackTree/src/Node.java
@@ -0,0 +1,59 @@
+
+public class Node {
+
+    private int data;
+
+    private Node parent;
+
+    private Node left;
+
+    private Node right;
+
+    private boolean isRed;
+
+    public Node(int data) {
+        this.data = data;
+        this.isRed = true;
+        parent = left = right = null;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    public boolean isRed() {
+        return isRed;
+    }
+
+    public void setRed(boolean red) {
+        isRed = red;
+    }
+}
