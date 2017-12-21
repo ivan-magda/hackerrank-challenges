bf1368dde6ea2e440cf7fdd49e3d2e08ec12b4cf
Ivan Magda <imagda15@gmail.com>
2017-12-21 13:31:08 +0300

    [code-reference/RBTree] chore: Create Tree class

---
+++ src/code-references/IMRedBlackTree/src/IMRedBlackTree.java
@@ -0,0 +1,21 @@
+
+public class IMRedBlackTree {
+
+    private Node root;
+
+    public IMRedBlackTree() {
+        this.root = null;
+    }
+
+    public IMRedBlackTree(Node root) {
+        this.root = root;
+    }
+
+    public IMRedBlackTree(int data) {
+        this(new Node(data));
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+}
